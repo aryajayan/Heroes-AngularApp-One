2cc86c08a3d0538b5ec22e1ff1990af10c1a58e6
Arya Jayan <aryajayan@users.noreply.github.com>
2020-03-26 03:10:17 +0530

    http added
    http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,22 +1,51 @@
 import { MessageService } from './message.service';
-import { HEROES } from './mock-heroes';
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService : Fetched Hero id =${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = this.heroesUrl + '/' + id;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`HeroService : Fetched Hero id =${id}`)),
+      catchError(this.handleError<Hero>('getHero id=' + id))
+    );
   }
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: Fetched heroes!');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('Fetched Heroes!')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
